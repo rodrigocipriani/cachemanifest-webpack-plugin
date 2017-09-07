fb544f9a9321ee3260814bbbaf872f6bfa084744
rodrigocipriani <chopsss@gmail.com>
2017-09-06 23:30:32 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# cachemanifest-webpack-plugin
+# cachemanifest-webpack-plugin 
 Webpack Plugin to generate CACHE MANIFEST 
+
+Script Copied from https://github.com/tomhicks-bsf/appcache-webpack-plugin. 
+The change is only this:
+
+Previously it was being generated like this:
+
+```
+    CACHE MANIFEST
+        
+    # 8a214e75b87d45313fa7
+    
+    app-8a214e75b87d45313fa7.bundle.js
+    appReact-8a214e75b87d45313fa7.bundle.js
+    vendors-8a214e75b87d45313fa7.js
+    vendors-style-8a214e75b87d45313fa7.css
+    index.html
+    
+    CACHE:
+    other.js
+    
+    NETWORK:
+    *
+```
+
+Is now being generated like this:
+
+```
+    CACHE MANIFEST
+
+    # 8a214e75b87d45313fa7
+    
+    CACHE:
+    other.js
+    app-8a214e75b87d45313fa7.bundle.js
+    appReact-8a214e75b87d45313fa7.bundle.js
+    vendors-8a214e75b87d45313fa7.js
+    vendors-style-8a214e75b87d45313fa7.css
+    index.html
+    
+    NETWORK:
+    *
+```
+
+## How to use
+
+Run `npm i --save-dev cachemanifest-webpack-plugin`
+
+```javascript
+
+var CacheManifestPlugin = require('appcache-webpack-plugin');
+
+module.exports = {
+  plugins: [
+    new CacheManifestPlugin({
+      cache: ['someOtherAsset.jpg'],
+      network: null,  // No network access allowed!
+      fallback: ['failwhale.jpg'],
+      settings: ['prefer-online'],
+      exclude: ['file.txt', /.*\.js$/]  // Exclude file.txt and all .js files
+      output: 'my-manifest.appcache'
+    })
+  ]
+}
+```
+
+Arguments:
+
+* `cache`: An array of additional assets to cache.
+* `network`: An array of assets that may be accessed via the network.
+  Defaults to `['*']`.
+* `fallback`: An array of fallback assets.
+* `settings`: An array of settings.
+* `exclude`: An array of strings or regex patterns. Assets in the compilation
+that match any of these patterns will be excluded from the manifest.
+* `output`: The filename to write the appcache to
+
+## License
+
+[MIT](http://www.opensource.org/licenses/mit-license.php)
+
---
+++ src/index.js
@@ -0,0 +1,81 @@
+class AppCache {
+
+  constructor(cache, network, fallback, settings, hash, comment) {
+    this.cache = cache;
+    this.network = network;
+    this.fallback = fallback;
+    this.settings = settings;
+    this.hash = hash;
+    this.comment = comment;
+    this.assets = [];
+  }
+
+  addAsset(asset) {
+    this.assets.push(encodeURI(asset));
+  }
+
+  size() {
+    return Buffer.byteLength(this.source(), 'utf8');
+  }
+
+  getManifestBody() {
+    return [
+      this.assets && this.assets.length ? `CACHE:\n${this.assets.join('\n')}` : null,
+      this.cache && this.cache.length ? `${this.cache.join('\n')}\n` : null,
+      this.network && this.network.length ? `NETWORK:\n${this.network.join('\n')}\n` : null,
+      this.fallback && this.fallback.length ? `FALLBACK:\n${this.fallback.join('\n')}\n` : null,
+      this.settings && this.settings.length ? `SETTINGS:\n${this.settings.join('\n')}\n` : null,
+    ].filter(v => v && v.length).join('\n');
+  }
+
+  source() {
+    return [
+      'CACHE MANIFEST',
+      `# ${this.hash}`,
+      this.comment || '',
+      this.getManifestBody(),
+    ].join('\n');
+  }
+}
+
+export default class CacheManifestPlugin {
+
+  static AppCache = AppCache
+
+  constructor({
+    cache,
+    network = ['*'],
+    fallback,
+    settings,
+    exclude = [],
+    output = 'manifest.appcache',
+    comment,
+  } = {}) {
+    this.cache = cache;
+    this.network = network;
+    this.fallback = fallback;
+    this.settings = settings;
+    this.output = output;
+    this.comment = comment ? `# ${comment}\n` : '';
+
+    // Convert exclusion strings to RegExp.
+    this.exclude = exclude.map(exclusion => {
+      if (exclusion instanceof RegExp) return exclusion;
+      return new RegExp(`^${exclusion}$`);
+    });
+  }
+
+  apply(compiler) {
+    const {options: {output: outputOptions = {}} = {}} = compiler;
+    const {publicPath = ''} = outputOptions;
+
+    compiler.plugin('emit', (compilation, callback) => {
+      const appCache = new AppCache(this.cache, this.network, this.fallback, this.settings, compilation.hash, this.comment);
+      Object.keys(compilation.assets)
+        .filter(asset => !this.exclude.some(pattern => pattern.test(asset)))
+        .forEach(asset => appCache.addAsset(publicPath + asset));
+      compilation.assets[this.output] = appCache;
+      callback();
+    });
+  }
+}
